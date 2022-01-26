48b20c6f56ad16c1cc7ea0ef4d822d685694e0c8
Reza Dadashi <94994554+RezaDadashi0@users.noreply.github.com>
2022-01-27 01:47:25 +0330

    Remove bootstrap and add tailwind pagination insted

--- src/Components/pagination.jsx
+++ src/Components/pagination.jsx
@@ -5,18 +5,22 @@ export default function Pagination({ length, pageSize, currentPage, onClick }) {
   let pages = _.range(1, Math.ceil(length / pageSize) + 1);
   if (pages.length === 1) pages = [];
 
-  console.log(pages);
+  let activeButton =
+    "py-2 px-4 mx-1 my-4 bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white transition ease-in duration-100 text-center text-base font-light shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg ";
+
+  let pasiveButton =
+    "py-2 px-4 mx-1 my-4 bg-white hover:bg-gray-100 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-indigo-500 transition ease-in duration-100 text-center text-base font-light shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 rounded-lg ";
 
   return (
-    <ul className="pagination pagination-md mt-4 d-flex justify-center">
+    <ul className="flex items-center justify-center">
       {pages.map((page, i) => (
         <li
           onClick={() => onClick(i + 1)}
           key={page}
           style={{ cursor: "pointer" }}
-          className={currentPage === i + 1 ? "page-item active" : "page-item"}
+          className={currentPage === i + 1 ? activeButton : pasiveButton}
         >
-          <div className="page-link">{page}</div>
+          {page}
         </li>
       ))}
     </ul>
